621a0e25d3a2a0b54b7ba4b6e13157cad2182566
Onur-DEMR <119036165+Onur-DEMR@users.noreply.github.com>
2023-02-10 14:46:25 +0300

    Hook dosyasının altında gece modu oluşturuldu ve App.js e bağlandı.

---
+++ frontend/components/hooks/geceModuAc.js
@@ -0,0 +1,6 @@
+import React, { useState } from "react";
+
+export default function geceModuAc(initialMode) {
+  const [geceModu, setGeceModu] = useState(initialMode);
+  return { geceModu, setGeceModu };
+}
